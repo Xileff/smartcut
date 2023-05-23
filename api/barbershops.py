ef3142392b78066806875405a6377432539ceb1c
Xileff <felixsavero78@gmail.com>
2023-05-23 23:22:49 +0700

    users can now update their barbershop

--- api/barbershops.py
+++ api/barbershops.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, make_response
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from services.BarbershopsService import *
+from utils.tokenize import verify_user_identity
 
 barbershop_route = Blueprint("barbershop_route", __name__)
 
@@ -11,16 +12,31 @@ def post_barbershop_handler():
     data = {
         "name": request.form.get("name"),
         "address": request.form.get("address"),
+        "picture": request.files.get("picture"),
         "latitude": request.form.get("latitude"),
         "longitude": request.form.get("longitude"),
         "user_id": get_jwt_identity(),
     }
 
-    picture = request.files.get("picture")
-
-    add_barbershop(data, picture)
+    add_barbershop(data)
 
     return make_response({"status": "success", "message": "Barbershop created!"})
+
+
+@barbershop_route.route("/barbershops/<string:uid>", methods=["PUT"])
+@jwt_required()
+def put_barbershop_handler(uid):
+    verify_user_identity(uid)
+    data = {
+        "name": request.form.get("name"),
+        "address": request.form.get("address"),
+        "picture": request.files.get("picture"),
+        "latitude": request.form.get("latitude"),
+        "longitude": request.form.get("longitude"),
+    }
+    edit_barbershop(uid, data)
+
+    return make_response({"status": "success", "message": "Barbershop edited"})
 
 
 @barbershop_route.route("/barbershops", methods=["GET"])
--- services/BarbershopsService.py
+++ services/BarbershopsService.py
@@ -1,13 +1,21 @@
 from models.Barbershop import Barbershop
-from werkzeug.exceptions import BadRequest, NotFound
+from models.IdCard import IdCard
+from models.User import User
+from werkzeug.exceptions import BadRequest, NotFound, Forbidden, Unauthorized
 from nanoid import generate
-from utils.storage import upload_picture
+from utils.storage import upload_picture, remove_file
 from utils.config import Config
 
 
-def add_barbershop(data, picture):
-    required_keys = ["name", "address", "latitude", "longitude", "user_id"]
-    name, address, latitude, longitude, user_id = data.values()
+def add_barbershop(data):
+    required_keys = ["name", "address", "picture", "latitude", "longitude", "user_id"]
+    name, address, picture, latitude, longitude, user_id = data.values()
+
+    user_id_card = IdCard.query.filter_by(user_id=user_id).first()
+    if not user_id_card:
+        raise Forbidden(
+            "You are not allowed to create a barbershop because you have not verified your identity yet"
+        )
 
     if not all(key in data for key in required_keys):
         raise BadRequest("All fields are required")
@@ -16,15 +24,43 @@ def add_barbershop(data, picture):
         raise BadRequest("All fields must not be empty")
 
     id = "barbershop" + generate(size=16)
-    picture = upload_picture(picture, Config.BARBERSHOP_PICTURE_PATH, id)
+    picture_url = upload_picture(picture, Config.BARBERSHOP_PICTURE_PATH, id)
 
     barbershop = Barbershop(
         id=id,
         name=name,
         address=address,
-        picture=picture,
+        picture=picture_url,
         latitude=latitude,
         longitude=longitude,
         user_id=user_id,
     )
+    barbershop.save()
+
+    user = User.query.filter_by(id=user_id).first()
+    user.is_barber = True
+    user.save()
+
+
+# todo
+def edit_barbershop(user_id, data):
+    barbershop = Barbershop.query.filter_by(user_id=user_id).first()
+    if not barbershop:
+        raise NotFound("Barbershop not found")
+
+    name, address, picture, latitude, longitude = data.values()
+
+    barbershop.name = name
+    barbershop.address = address
+    barbershop.latitude = latitude
+    barbershop.longitude = longitude
+
+    if picture:
+        previous_picture = barbershop.picture.split("/")[-1]
+        remove_file(Config.BARBERSHOP_PICTURE_PATH, previous_picture)
+        public_url = upload_picture(
+            picture=picture, path=Config.BARBERSHOP_PICTURE_PATH, filename=barbershop.id
+        )
+        barbershop.picture = public_url
+
     barbershop.save()

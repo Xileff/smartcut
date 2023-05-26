97535f821996293ceefad9386ce21b880acad72c
Xileff <felixsavero78@gmail.com>
2023-05-26 09:42:16 +0700

    Users can now review appointments

--- api/reviews.py
+++ api/reviews.py
@@ -1,4 +1,28 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, make_response
+from flask_jwt_extended import jwt_required
+from werkzeug.exceptions import BadRequest
+from utils.tokenize import verify_user_identity
 from services.ReviewsService import *
 
 review_route = Blueprint("reviews_route", __name__)
+
+
+@review_route.route("/reviews", methods=["POST"])
+@jwt_required()
+def post_review_handler():
+    data = request.json
+
+    if not data["userId"]:
+        raise BadRequest("User id is required")
+
+    user_id = data["userId"]
+    verify_user_identity(user_id)
+
+    message = add_review(data)
+
+    return make_response(
+        {
+            "status": "success",
+            "message": message,
+        }
+    )
--- services/ReviewsService.py
+++ services/ReviewsService.py
@@ -1,1 +1,35 @@
+from werkzeug.exceptions import BadRequest, NotFound
+from nanoid import generate
 from models.Review import Review
+from models.Appointment import Appointment
+
+
+def add_review(data):
+    required_keys = ["stars", "message", "appointmentId"]
+    if not all([key in data for key in required_keys]):
+        raise BadRequest("All fields are required")
+
+    stars = data["stars"]
+    message = data["message"]
+    appointment_id = data["appointmentId"]
+
+    appointment = Appointment.query.filter_by(id=appointment_id).first()
+
+    if not appointment:
+        raise NotFound("Invalid appointment id")
+
+    if not appointment.is_finished:
+        raise BadRequest("This appointment is not finished yet")
+
+    review_id = generate(size=16)
+    review = Review(
+        id=review_id,
+        stars=stars,
+        message=message,
+        appointment_id=appointment_id,
+    )
+
+    review.save()
+
+    message = appointment_id + " reviewed succesfully"
+    return message
